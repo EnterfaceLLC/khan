0849186de336f894f360abbbdebe19749fe0c056
EnterfaceLLC <dotenterface@gmail.com>
2024-01-25 21:49:35 -0600

    Inventory Styles update

--- src/screens/inventory/inventoryScrn.js
+++ src/screens/inventory/inventoryScrn.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import { styles, andriod } from './styles';
 
 const InventoryScrn = () => {
     return(
-        <View>
+        <View style={[styles.mainContnr, andriod]}>
             <Text>Inventory Screen</Text>
         </View>
     );
