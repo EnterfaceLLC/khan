85f543d27d41362fbebcd4a04107a0d0670b8636
EnterfaceLLC <dotenterface@gmail.com>
2024-01-25 21:18:13 -0600

    +Inventory Folder

---
+++ src/screens/inventory/inventoryScrn.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const InventoryScrn = () => {
+    return(
+        <View>
+            <Text>Inventory Screen</Text>
+        </View>
+    );
+};
+
+export default InventoryScrn;
